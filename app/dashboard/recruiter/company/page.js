dbae14e8f3dd2e29321ae0e5e06ad961153fe3d2
anaitullah31 <anaitullah198@gmail.com>
2026-06-08 17:02:51 +0800

    Design jobs and details page

--- app/dashboard/recruiter/company/page.js
+++ app/dashboard/recruiter/company/page.js
@@ -1,7 +1,17 @@
 import React from "react";
+import CompanyProfile from "./CompanyProfile";
+import { getUserSession } from "@/app/lib/core/session";
+import { getRecruiterCompany } from "@/app/lib/api/companies";
 
-const CompanyPage = () => {
-  return <div>Company Page</div>;
+const CompanyPage = async () => {
+  const user = await getUserSession();
+  const company = await getRecruiterCompany(user?.id);
+
+  return (
+    <div>
+      <CompanyProfile recruiter={user} recruiterCompany={company} />
+    </div>
+  );
 };
 
 export default CompanyPage;
---
+++ app/lib/api/companies.js
@@ -0,0 +1,17 @@
+import { serverFetch } from "../core/server";
+import { getUserSession } from "../core/session";
+
+// const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const getRecruiterCompany = async (recruiterId) => {
+  return serverFetch(`/api/my/companies?recruiterId=${recruiterId}`);
+  //   const res = await fetch(
+  //     `${baseUrl}/api/my/companies?recruiterId=${recruiterId}`,
+  //   );
+  //   return res.json();
+};
+
+export const getLoggedInRecruiterCompany = async () => {
+  const user = await getUserSession();
+  return getRecruiterCompany(user?.id);
+};
