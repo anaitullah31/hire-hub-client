39a31e736f323954537ea37c413a458cbcffedba
anaitullah31 <anaitullah198@gmail.com>
2026-06-14 10:24:28 +0800

    implement jwt

--- app/lib/core/server.js
+++ app/lib/core/server.js
@@ -1,15 +1,33 @@
+import { getUserToken } from "./session";
+
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const authHeader = async () => {
+  const token = await getUserToken();
+  const header = {
+    authorization: `Bearer ${token}`,
+  };
+  return token ? header : {};
+};
 
 export const serverFetch = async (path) => {
   const res = await fetch(`${baseUrl}${path}`);
   return res.json();
 };
 
-export const serverMutation = async (path, data, method="POST") => {
+export const protectedFetch = async (path) => {
+  const res = await fetch(`${baseUrl}${path}`, {
+    headers: await authHeader(),
+  });
+  return res.json();
+};
+
+export const serverMutation = async (path, data, method = "POST") => {
   const res = await fetch(`${baseUrl}${path}`, {
     method: method,
     headers: {
       "Content-Type": "application/json",
+      ...(await authHeader()),
     },
     body: JSON.stringify(data),
   });
